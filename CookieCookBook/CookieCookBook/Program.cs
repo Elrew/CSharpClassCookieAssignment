823b57cba99e84d8f61d73986e29423d04218151
Jake <71288819+Elrew@users.noreply.github.com>
2024-08-15 21:27:06 -0700

    Add files via upload

---
+++ CookieCookBook/CookieCookBook/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CookieCookBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(110, 50);
+            WriteBorder();
+            Console.ReadKey();
+
+        }
+
+        static void WriteBorder()
+        {
+            //top 
+            for(int i = 0; i <=109 ; i++)
+            {
+                Console.SetCursorPosition(i,0);
+                Console.WriteLine("#");
+            }
+            //bottom
+            for (int i = 0; i <= 109; i++)
+            {
+                Console.SetCursorPosition(i, 48);
+                Console.WriteLine("#");
+            }
+            //left
+            for (int i = 0; i <= 48; i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.WriteLine("#");
+            }
+
+
+        }
+    }
+}
